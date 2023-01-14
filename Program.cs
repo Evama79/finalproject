99f34732dc2b8c5131ac8746e1f303cf7b2b9c4c
Evama79 <mandicevgenija79@gmail.com>
2023-01-14 17:27:28 +0700

    initial task commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 ﻿namespace Ns {
     public class Tests {
         
+        static string[] Filter(string[] arr) {
+            string[] buffer = new string[arr.Length];
+            int i = 0;
+            foreach(string item in arr) {
+                if(item.Length <= 3) {
+                    buffer[i] = item;
+                    i++;
+                }
+            }
+            return buffer;
+        }
         public static void Main(string[] args) {
-            
+            string[] test = {"asd", "sdfg", "qwe", "a143", "ghruaf", "234"};
+            List<string> ls = new List<string>();
+
+            foreach(string item in Filter(test)) {
+                Console.Write("{0}\t", item);
+            }
         }
     }
 }
